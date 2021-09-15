a8ee01e581c25a211576440adde2fe73acc7ea5f
johnnymeneses <bilbo_vader@outlook.com>
2021-09-15 13:31:37 -0300

    Imersão Aula 1

---
+++ ConversorMoedas/calculadora.js
@@ -0,0 +1,11 @@
+
+var nota1 = 10
+var nota2 = 8
+var nota3 = 7
+var nota4 = 5
+
+var notaFinal = (nota1+nota2+nota3+nota4) / 4
+
+var notaAjustada= notaFinal.toFixed(0)
+
+console.log("Nota Final"+notaAjustada)
